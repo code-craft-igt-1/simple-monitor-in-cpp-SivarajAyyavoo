6618fbc257ebf37330975afdf8d586ed540c10de
A <Sivaraj.A@philips.com>
2024-09-04 09:39:46 +0530

    Update test to get 100% coverage - 1

--- test-health_monitor.cpp
+++ test-health_monitor.cpp
@@ -4,8 +4,27 @@
 
 namespace health_monitor {
 namespace test {
-    TEST(Monitor, NotOkWhenAnyVitalIsOffRange) {
-        ASSERT_FALSE(vitalsOk(99, 102, 70));
+    TEST(HealthMonitor, NotOkWhenTempOffRange) {
+        // Test the Lower bound
+        ASSERT_FALSE(vitalsOk(90, 90, 95));
+        // Test the Upper bound
+        ASSERT_FALSE(vitalsOk(110, 90, 95));
+    }
+
+    TEST(HealthMonitor, NotOkWhenPulseRateIsOffRange) {
+        // Test the Lower bound
+        ASSERT_FALSE(vitalsOk(99, 55, 95));
+        // Test the Upper bound
+        ASSERT_FALSE(vitalsOk(99, 105, 95));
+    }
+
+    TEST(HealthMonitor, NotOkWhenSpo2IsOffRange) {
+        // Spo2 can be maximum of 100, so testing only its lower bound
+        ASSERT_FALSE(vitalsOk(99, 90, 80));
+    }
+
+    TEST(HealthMonitor, OkWhenAllVitalsAreInRange)
+    {
         ASSERT_TRUE(vitalsOk(98.1, 70, 98));
     }
 }  // namespace test
